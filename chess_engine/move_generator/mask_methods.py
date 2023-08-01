2e568ebca2ca7de78475a63d1d4282df80ba5420
Ariana <arianahejazyan@gmail.com>
2023-08-01 14:10:18 +0330

    Add comments

--- chess_engine/move_generator/mask_methods.py
+++ chess_engine/move_generator/mask_methods.py
@@ -4,19 +4,22 @@ def mask_king_attacks(square: int) -> int:
     """
     Generates a bitboard representing the possible king attacks from a given square on a chess board.
     """
+
     mask = 0
 
+    # calculate the rank (r) and the file (f) of the square on the chessboard
     r = square // 14
     f = square % 14
 
-    if (r < 13):            mask ^= (1 << square + 14) 
-    if (r < 13) & (f < 13): mask ^= (1 << square + 15)
-    if (f < 13):            mask ^= (1 << square + 1 )
-    if (r > 0 ) & (f < 13): mask ^= (1 << square - 13)
-    if (r > 0 )           : mask ^= (1 << square - 14)
-    if (r > 0 ) & (f > 0 ): mask ^= (1 << square - 15)
-    if (f > 0 )           : mask ^= (1 << square - 1 )
-    if (r < 13) & (f > 0 ): mask ^= (1 << square + 13)
+    # calculate attack mask for each possible king move (8 directions)
+    if (r < 13)           : mask ^= (1 << square + 14) # Up
+    if (r < 13) & (f < 13): mask ^= (1 << square + 15) # Up-Right
+    if (f < 13)           : mask ^= (1 << square + 1 ) # Right
+    if (r > 0 ) & (f < 13): mask ^= (1 << square - 13) # Down-Right
+    if (r > 0 )           : mask ^= (1 << square - 14) # Down
+    if (r > 0 ) & (f > 0 ): mask ^= (1 << square - 15) # Down-Left
+    if (f > 0 )           : mask ^= (1 << square - 1 ) # Left
+    if (r < 13) & (f > 0 ): mask ^= (1 << square + 13) # Up-Left
 
     return mask
 
@@ -27,17 +30,19 @@ def mask_knight_attacks(square: int) -> int:
 
     mask = 0
 
+    # calculate the rank (r) and the file (f) of the square on the chessboard
     r = square // 14
     f = square % 14
 
-    if (r < 12) & (f < 13): mask ^= (1 << square + 29) 
-    if (r < 13) & (f < 12): mask ^= (1 << square + 16)
-    if (r > 0 ) & (f < 12): mask ^= (1 << square - 12)
-    if (r > 1 ) & (f < 13): mask ^= (1 << square - 27)
-    if (r > 1 ) & (f > 0 ): mask ^= (1 << square - 29)
-    if (r > 0 ) & (f > 1 ): mask ^= (1 << square - 16)
-    if (r < 13) & (f > 1 ): mask ^= (1 << square + 12)
-    if (r < 12) & (f > 0 ): mask ^= (1 << square + 27)
+    # calculate attack mask for each possible king move (8 directions in L-shape)
+    if (r < 12) & (f < 13): mask ^= (1 << square + 29) # Up-Up-Right
+    if (r < 13) & (f < 12): mask ^= (1 << square + 16) # Up-Right-Right
+    if (r > 0 ) & (f < 12): mask ^= (1 << square - 12) # Down-Right-Right
+    if (r > 1 ) & (f < 13): mask ^= (1 << square - 27) # Down-Down-Right
+    if (r > 1 ) & (f > 0 ): mask ^= (1 << square - 29) # Down-Down-Left
+    if (r > 0 ) & (f > 1 ): mask ^= (1 << square - 16) # Down-Left-Left
+    if (r < 13) & (f > 1 ): mask ^= (1 << square + 12) # Up-Left-Left
+    if (r < 12) & (f > 0 ): mask ^= (1 << square + 27) # Up-Up-Left
 
     return mask
 
@@ -48,20 +53,22 @@ def mask_pawn_attacks(square: int, player: int) -> int:
 
     mask = 0
 
+    # calculate the rank (r) and the file (f) of the square on the chessboard
     r = square // 14
     f = square % 14
 
+    # calculate attack mask for each possible pawn capture based on the player's direction
     if player == 0:
-        if (r < 13) & (f > 0 ): mask ^= (1 << square + 13) 
-        if (r < 13) & (f < 13): mask ^= (1 << square + 15)
+        if (r < 13) & (f > 0 ): mask ^= (1 << square + 13) # Up-Left
+        if (r < 13) & (f < 13): mask ^= (1 << square + 15) # Up-Right
     elif player == 1:
-        if (r < 13) & (f < 13): mask ^= (1 << square + 15) 
-        if (r > 0 ) & (f < 13): mask ^= (1 << square - 13)
+        if (r < 13) & (f < 13): mask ^= (1 << square + 15) # Up-Right
+        if (r > 0 ) & (f < 13): mask ^= (1 << square - 13) # Down-Right
     elif player == 2:
-        if (r > 0 ) & (f < 13): mask ^= (1 << square - 13)
-        if (r > 0 ) & (f > 0 ): mask ^= (1 << square - 15)
+        if (r > 0 ) & (f < 13): mask ^= (1 << square - 13) # Down-Right
+        if (r > 0 ) & (f > 0 ): mask ^= (1 << square - 15) # Down-Left
     else:
-        if (r > 0 ) & (f > 0 ): mask ^= (1 << square - 15) 
-        if (r < 13) & (f > 0 ): mask ^= (1 << square + 13)
+        if (r > 0 ) & (f > 0 ): mask ^= (1 << square - 15) # Down-Left
+        if (r < 13) & (f > 0 ): mask ^= (1 << square + 13) # Up-Left
 
     return mask
