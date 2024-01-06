dd754096e2f5365c9fe7839775ae700058f7d8cc
Ariana <arianahejazyan@gmail.com>
2024-01-06 20:51:34 +0330

    Add new variables

--- Nyx/utils/constants.h
+++ Nyx/utils/constants.h
@@ -3,9 +3,17 @@
 
 namespace
 {
+    constexpr int ROWSIZE = 14;
+    constexpr int COLSIZE = 14;
+
+    constexpr int MAXRANK = ROWSIZE - 1;
+    constexpr int MAXFILE = COLSIZE - 1;
+
     vector<Square> VALID_SQUARES;
 
     vector<Piece> PIECE_LIST;
 
     map<Piece, vector<int>> PIECE_DIRECTIONS;
+
+    map<Piece, vector<Offset>> PIECE_RAYS;
 };
