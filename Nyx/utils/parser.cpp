bc4850c28dcbc296af50c5fb5eb063ed44cfb2fe
Ariana <arianahejazyan@gmail.com>
2024-01-10 11:52:31 +0330

    Initial commit

---
+++ Nyx/utils/parser.cpp
@@ -0,0 +1,21 @@
+#include "parser.h"
+
+Config Parser::parse_config(const string& png4)
+{
+    Config cfg;
+
+
+
+
+    return cfg;
+}
+
+Position Parser::parse_position(const string& png4)
+{
+    Position pos;
+
+
+
+
+    return pos;
+}
