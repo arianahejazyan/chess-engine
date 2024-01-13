e9de1c00c1584709d474b75e743fe732a84a595e
Ariana <arianahejazyan@gmail.com>
2024-01-13 14:17:20 +0330

    Add new lookup tables

--- Nyx/utils/constants.h
+++ Nyx/utils/constants.h
@@ -1,13 +1,19 @@
 #pragma once
 #include "definitions.h"
 
-namespace
+namespace CONSTANTS
 {
     constexpr int ROWSIZE = 14;
     constexpr int COLSIZE = 14;
 
     constexpr int MAXRANK = ROWSIZE - 1;
     constexpr int MAXFILE = COLSIZE - 1;
+
+    map<Piece, string> PIECE_TO_STRING;
+
+    map<string, Piece> STRING_TO_PIECE;
+
+    map<Square, string> SQUARE_TO_STRING;
 
     vector<Square> VALID_SQUARES;
 
@@ -16,6 +22,10 @@ namespace
     map<Piece, vector<int>> PIECE_DIRECTIONS;
 
     map<Piece, vector<Offset>> PIECE_RAYS;
-};
 
-void init_valid_squares();
+    void init();
+
+    void init_piece_to_string();
+
+    void INIT_STRING_TO_PIECE();
+};
