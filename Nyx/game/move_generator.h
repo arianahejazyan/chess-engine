ad186aed6fb908e70912affd4090f81524ca2bd1
Ariana <arianahejazyan@gmail.com>
2024-02-24 21:06:59 +0330

    Change move to int

--- Nyx/game/move_generator.h
+++ Nyx/game/move_generator.h
@@ -6,23 +6,23 @@ class MoveGenerator
 {
     public:
 
-        void generate_all_moves(const Position& pos, vector<Move>& move_list);
+        void generate_all_moves(const Position& pos, vector<int>& move_list);
 
     protected:
 
-        void generate_king_moves(const Position& pos, const Square& sq, vector<Move>& move_list);
+        void generate_king_moves(const Position& pos, const Square& sq, vector<int>& move_list);
 
-        void generate_queen_moves(const Position& pos, const Square& sq, vector<Move>& move_list);
+        void generate_queen_moves(const Position& pos, const Square& sq, vector<int>& move_list);
 
-        void generate_rook_moves(const Position& pos, const Square& sq, vector<Move>& move_list);
+        void generate_rook_moves(const Position& pos, const Square& sq, vector<int>& move_list);
 
-        void generate_bishop_moves(const Position& pos, const Square& sq, vector<Move>& move_list);
+        void generate_bishop_moves(const Position& pos, const Square& sq, vector<int>& move_list);
 
-        void generate_knight_moves(const Position& pos, const Square& sq, vector<Move>& move_list);
+        void generate_knight_moves(const Position& pos, const Square& sq, vector<int>& move_list);
 
-        void generate_pawn_moves(const Position& pos, const Square& sq, vector<Move>& move_list);
+        void generate_pawn_moves(const Position& pos, const Square& sq, vector<int>& move_list);
 
         /* ---------------------------------------------------------------------------- */
 
-        void add_move(const Position& pos, Move move, vector<Move>& move_list);
+        void add_move(const Position& pos, int move, vector<int>& move_list);
 };
