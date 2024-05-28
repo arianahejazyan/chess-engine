949ec3f7b90e8aa6c496b220f7c8bb4728c6b10b
fnb2002 <fatemehfnb26@gmail.com>
2024-05-28 22:42:56 +0330

    Initial commit

---
+++ talia/utils/parser.py
@@ -0,0 +1,5 @@
+std = 'R-0,0,0,0-1,1,1,1-1,1,1,1-0,0,0,0-0-x,x,x,yR,yN,yB,yK,yQ,yB,yN,yR,x,x,x/x,x,x,yP,yP,yP,yP,yP,yP,yP,yP,x,x,x/x,x,x,8,x,x,x/bR,bP,10,gP,gR/bN,bP,10,gP,gN/bB,bP,10,gP,gB/bQ,bP,10,gP,gK/bK,bP,10,gP,gQ/bB,bP,10,gP,gB/bN,bP,10,gP,gN/bR,bP,10,gP,gR/x,x,x,8,x,x,x/x,x,x,rP,rP,rP,rP,rP,rP,rP,rP,x,x,x/x,x,x,rR,rN,rB,rQ,rK,rB,rN,rR,x,x,x'
+
+def parse(fen):
+    s = fen.split('-')
+    return s
