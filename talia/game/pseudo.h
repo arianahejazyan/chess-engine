d8dfdf87fe7068f46aba370f8b7b776199034ed1
Ariana Hejazyan <arianahejazyan@gmail.com>
2024-07-09 11:43:22 +0330

    Clean up

--- talia/game/pseudo.h
+++ talia/game/pseudo.h
@@ -8,6 +8,8 @@
 
 typedef int Key;
 
+/* ---------------------------------------------------------------------------- */
+
 #define hashPlayerSide(player, side) (player << OFFSET::SIDE_BITS ^ side)
 #define hashSquarePiece(square, piece) (square << PIECE::PIECE_BITS ^ piece)
 #define hashSquarePieceRay(square, piece, ray) (((square << PIECE::PIECE_BITS ^ piece) << OFFSET::RAY_BITS) ^ ray)
@@ -17,42 +19,35 @@ typedef int Key;
 
 namespace PSEUDO
 {
+    // contains all pseudo legal moves //
     std::unordered_map<Key, std::vector<Move>> LEAP;
     std::unordered_map<Key, std::vector<Move>> SLIDE;
     std::unordered_map<Key, std::vector<Move>> PUSH;
     std::unordered_map<Key, std::vector<Move>> ADVANCE;
 
+    // contains all castle moves and related squares //
     std::unordered_map<Key, Move> CASTLE;
-
     std::unordered_map<Key, std::vector<Square>> SECURE_SQUARES;
     std::unordered_map<Key, std::vector<Square>> PASSING_SQUARES;
 
     std::unordered_map<Key, std::vector<Ray>> RAY;
 
-
-
     // contains all leap offsets //
-    std::vector<std::pair<Offset, Square>> LEAP_OFFSETS[BOARD::BOARDSIZE];
+    std::vector<std::pair<Offset, Square>> LEAP_OFFSETS[BOARD::BOARD_SIZE];
 
     // contains all slide offsets //
-    std::vector<Offset> SLIDE_OFFSETS[BOARD::BOARDSIZE];
-    std::unordered_map<Offset, std::vector<Square>> SLIDE_RAY[BOARD::BOARDSIZE];
+    std::vector<Offset> SLIDE_OFFSETS[BOARD::BOARD_SIZE];
+    std::unordered_map<Offset, std::vector<Square>> SLIDE_RAY[BOARD::BOARD_SIZE];
 
     // contains all advance offsets //
-    std::vector<std::pair<Offset, Square>> ADVANCE_OFFSETS[BOARD::BOARDSIZE][PLAYER::MAX_PLAYERS];
+    std::vector<std::pair<Offset, Square>> ADVANCE_OFFSETS[BOARD::BOARD_SIZE][PLAYER::MAX_PLAYERS];
 
     //  //
     std::unordered_map<Offset, bool> VALID_LEAP[PIECE::MAX_PIECES];
     std::unordered_map<Offset, bool> VALID_SLIDE[PIECE::MAX_PIECES];
     std::unordered_map<Offset, bool> VALID_ADVANCE[PIECE::MAX_PIECES];
 
-    
-
-    
-
-
-    
-
+    /********************************************************************************/
 
     void leap(const Square& sq, const Piece& piece, const Direction& dir);
 
