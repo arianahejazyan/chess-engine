ccac9f632c114df8aa42ce5e0a41b8b9c9a77639
Ariana Hejazyan <arianahejazyan@gmail.com>
2024-07-10 10:25:44 +0330

    Config

--- talia/setting/config.h
+++ talia/setting/config.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <vector>
-#include "piece.h"
+//#include "piece.h"
 
 /* ---------------------------------------------------------------------------- */
 
@@ -10,34 +10,38 @@ enum StaleMate {Draw, Win, Lose};
 
 enum Setup {FFA, RB, RY, RG};
 
-enum Mode {4PC, 2PC};
+//enum Mode {4PC, 2PC};
 
 /* ---------------------------------------------------------------------------- */
 
 namespace CONFIG
 {
-    bool allow_passing;
-    bool sideways;
-    bool torpedo;
-    bool any_capture;
-    bool capture_the_king;
-    bool enpassant;
-    bool play_for_mate; // ffa
-    bool zombies; // ffa
-    bool game_of_points; // 2pc
+    extern bool capture_the_king;
+    extern bool game_of_points;
+    extern bool allow_passing;
+    extern bool play_for_mate;
+    extern bool any_capture;
+    extern bool no_enpassant;
+    extern bool sideways;
+    extern bool torpedo;
+    extern bool zombies;
+    
+    extern int opposite_multiplier;
+    extern int time_control_value;
+    extern int points_for_mate;
+    extern int promotion_rank;
+    extern int home_rank;
+    extern int base_time;
 
-    int promotion_rank;
+    // StaleMate stalemate;
+    
+    
+    // TimeControl time_control;
+    
 
-    int home_rank;
-    StaleMate stalemate;
-    int opposite_multiplier; // ffa
-    int points_for_mate; // ffa
-    TimeControl time_control;
-    int time_control_value;
+    // Setup setup;
 
-    Setup setup;
-
-    void init();
+    void print();
 };
 
 /* ---------------------------------------------------------------------------- */
