a4c56c99ae37aebe89fa801113f6edf96a242ba7
Ariana Hejazyan <arianahejazyan@gmail.com>
2024-09-05 11:57:12 +0330

    refactor: entry point

--- talia/main.py
+++ talia/main.py
@@ -1,25 +1,35 @@
-from interface.command_line import CommandLine
-from interface.interpreter import Interpreter
-from parsers.piece_parser import PieceParser
+import sys
+from PyQt6.QtWidgets import QApplication
 
-class Application:
+from parsers import *
+from interface.CLI import *
+from interface.GUI.windows import *
+
+class Talia:
 
     def __init__(self):
 
+        self.app = QApplication(sys.argv)
+
+
+        self.terminal_window = TerminalWindow(self)
+        self.board_window = BoardWindow(self)
+        self.edit_window = EditWindow(self)
+        self.analyze_window = AnalyzeWindow(self)
+
         self.interpreter = Interpreter(self)
-        self.command_line = CommandLine(self)
-        #self.piece_parser = PieceParser()
-        #self.fen_parser = FenParser()
-
-    def _create_components(self):
-
-        pass
-    
-    def run(self):
+        self.piece_parser = PieceParser(self)
+        self.fen_parser = FENParser(self)
+        self.pgn_parser = PGNParser(self)
         
-        self.command_line.run()
+
+    def start(self):
+
+        
+        self.terminal_window.show()
+        sys.exit(self.app.exec())
 
 if __name__ == '__main__':
 
-    app = Application()
-    app.run()
+    program = Talia()
+    program.start()
