76ea1775ce450ef815e7666a793c49dae4aad996
Ariana Hejazyan <arianahejazyan@gmail.com>
2024-09-05 13:35:31 +0330

    chore: add __initi__.py

---
+++ talia/interface/GUI/windows/__init__.py
@@ -0,0 +1,6 @@
+from .analyze_window import AnalyzeWindow
+from .board_window import BoardWindow
+from .edit_window import EditWindow
+from .terminal_window import TerminalWindow
+
+__all__ = ['AnalyzeWindow', 'BoardWindow', 'EditWindow', 'TerminalWindow']
---
+++ talia/parsers/__init__.py
@@ -0,0 +1,5 @@
+from .piece_parser import PieceParser
+from .fen_parser import FENParser
+from .pgn_parser import PGNParser
+
+__all__ = ['PieceParser', 'FENParser', 'PGNParser']
