3eaaa209296b88373abad0de7d3267755749a898
Ariana Hejazyan <arianahejazyan@gmail.com>
2024-09-05 13:34:09 +0330

    refactor: change board window title

--- talia/interface/GUI/windows/board_window.py
+++ talia/interface/GUI/windows/board_window.py
@@ -6,7 +6,7 @@ class BoardWindow(QWidget):
         super().__init__()
         self.app = app
 
-        self.setWindowTitle("Board Window")
+        self.setWindowTitle("Board")
 
         self.setGeometry(300, 300, 600, 400)
         self.setStyleSheet("background-color: #161616")
