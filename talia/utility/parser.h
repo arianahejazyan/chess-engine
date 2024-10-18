b6d5c479cd2d0034b8b99d494500288635697154
Ariana Hejazyan <arianahejazyan@gmail.com>
2024-10-18 09:56:03 +0330

    feat: add method signitures

--- talia/utility/parser.h
+++ talia/utility/parser.h
@@ -1,18 +1,26 @@
 #include "definitions.h"
 
+using namespace definitions;
+
 namespace parser
 {
+    bool is_integer(const string& str);
+    
     vector<string> split_string(const string& fen, char delimiter);
 
     vector<string> split_board(const string& board);
+
+    bool parse_piece(char ch, Piece& piece);
+
+    bool parse_player(char ch, Player& player);
+
+    bool parse_board(const string& board, Position& pos);
 
     // bool parse_turn(const string& turn, Position& pos);
 
     //bool parse_right(const string& right, const Side& side, Position& pos);
 
     //bool parse_enpassant(const string& enpassant, Position& pos);
-
-    // bool parse_board(const string& board, Position& pos);
 
     // bool parse_fen(const string& fen, Position& pos);
 };
