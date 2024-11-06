86401cdf8f2838b37dfac4a38d4f6ed8ed512aa3
Ariana Hejazyan <arianahejazyan@gmail.com>
2024-11-06 18:31:24 +0330

    test: add queen rook bishop pseudo moves tests

--- talia/tests/test_pseudo.cc
+++ talia/tests/test_pseudo.cc
@@ -49,6 +49,34 @@ TEST_F(PseudoTest, Knight)
     check(159, KNIGHT_MOVEMENT, {149, 142});
 }
 
+TEST_F(PseudoTest, Queen)
+{
+    check(28, QUEEN_MOVEMENT[0], {43, 58, 73, 88, 103, 118, 133});
+    check(28, QUEEN_MOVEMENT[1], {42, 56, 70, 84, 98, 112, 126, 137, 145, 153});
+    check(28, QUEEN_MOVEMENT[2], {41, 54, 67, 80});
+    check(28, QUEEN_MOVEMENT[3], {29, 30, 31, 32, 33, 34, 35, 36, 37});
+    check(28, QUEEN_MOVEMENT[4], {27, 26, 25, 24});
+    check(28, QUEEN_MOVEMENT[5], {18, 11, 4});
+    check(28, QUEEN_MOVEMENT[6], {17, 9, 1});
+    check(28, QUEEN_MOVEMENT[7], {16});
+}
+
+TEST_F(PseudoTest, Rook)
+{
+    check(100, ROOK_MOVEMENT[0], {114, 128, 139, 147, 155});
+    check(100, ROOK_MOVEMENT[1], {101, 102, 103, 104, 105, 106, 107});
+    check(100, ROOK_MOVEMENT[2], {99, 98, 97, 96, 95, 94});
+    check(100, ROOK_MOVEMENT[3], {86, 72, 58, 44, 30, 19, 11, 3});
+}
+
+TEST_F(PseudoTest, Bishop)
+{
+    check(59, BISHOP_MOVEMENT[0], {74, 89, 104, 119, 134});
+    check(59, BISHOP_MOVEMENT[1], {72, 85, 98, 111, 124});
+    check(59, BISHOP_MOVEMENT[2], {46, 33, 23});
+    check(59, BISHOP_MOVEMENT[3], {44, 29, 17, 8});
+}
+
 /*
 TEST_F(PseudoTest, Slide)
 {
